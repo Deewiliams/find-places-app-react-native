b117a78884518f946731d055176372ae8b3b8758
Desire <desireirankundawilliams@gmail.com>
2023-09-26 14:00:20 +0200

    added lat and long for the map

--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,11 +1,22 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
-import MapView from "react-native-maps";
-import React from "react";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useState } from "react";
 
 export default function GoogleMapView() {
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 28.398297,
+    longitude: -15.3653549,
+    latitudeDelta: 0.0522,
+    longitudeDeltal: 0.0421,
+  });
   return (
-    <View>
-      <MapView style={styles.map} />
+    <View style={{ marginTop: 20 }}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        region={mapRegion}
+      />
     </View>
   );
 }
@@ -17,5 +28,6 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("screen").width * 0.89,
     height: Dimensions.get("screen").height * 0.23,
+    borderRadius: 20,
   },
 });
