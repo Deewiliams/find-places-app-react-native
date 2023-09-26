115e12037e5310d9a2fbe6593bc707a6621ded7b
Desire <desireirankundawilliams@gmail.com>
2023-09-26 13:10:40 +0200

    created a shared file

--- App/Components/Home/Header.js
+++ App/Components/Home/Header.js
@@ -1,10 +1,18 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TextInput, Dimensions } from "react-native";
 import React from "react";
+import Colors from "../../Shared/Colors";
 
 export default function Header() {
   return (
-    <View>
+    <View style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center"}}>
       <Image style={styles.logo} source={require("../../../assets/logo.png")} />
+      <View>
+        <TextInput placeholder="search" style={styles.searchBar} />
+      </View>
+      <Image
+        style={styles.userImage}
+        source={require("../../../assets/placeholder.jpg")}
+      />
     </View>
   );
 }
@@ -12,5 +20,18 @@ const styles = StyleSheet.create({
   logo: {
     width: 50,
     height: 50,
+  },
+  searchBar: {
+    borderWidth: 1,
+    borderColor: Colors.black,
+    padding: 4,
+    borderRadius: 50,
+    paddingLeft: 10,
+    width:Dimensions.get('screen').width*0.6,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 100 
   },
 });
