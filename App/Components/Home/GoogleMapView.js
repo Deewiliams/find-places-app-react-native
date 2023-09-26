0fb2ca09d39ca7a3ec52c3238f79da3dae35ad72
Desire <desireirankundawilliams@gmail.com>
2023-09-26 19:32:50 +0200

    created category list page

--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,40 +1,49 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { Marker } from "react-native-maps";
 import React, { useContext, useEffect, useState } from "react";
 import { UserLocationContext } from "../../Context/UserLocationcontext";
 
 export default function GoogleMapView() {
   const [mapRegion, setmapRegion] = useState();
-  const {location, setLocation} = useContext(UserLocationContext)
+  const { location, setLocation } = useContext(UserLocationContext);
 
-  
   useEffect(() => {
-    if(location){
+    if (location) {
       setmapRegion({
         latitude: location?.coords?.latitude,
         longitude: location?.coords?.longitude,
         latitudeDelta: 0.0522,
         longitudeDeltal: 0.0421,
-      })
+      });
     }
-   
-  }, [])
-  console.log("app 45 location", location?.coords?.latitude);
-  
-  
+  }, []);
+
   return (
-    <View style={{ marginTop: 20, borderRadius: 20}}>
-     <View>
-     <Text style={{fontSize: 20, marginBottom: 10, fontWeight: "600", fontFamily: "Raleway-bold"}}>
-      Top near by places
-    </Text>
-     </View>
+    <View style={{ marginTop: 20, borderRadius: 20 }}>
+      <View>
+        <Text
+          style={{
+            fontSize: 20,
+            marginBottom: 10,
+            fontWeight: "600",
+            fontFamily: "Raleway-bold",
+          }}
+        >
+          Top near by places
+        </Text>
+      </View>
       <MapView
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
         region={mapRegion}
-      />
+      >
+        <Marker 
+        title="you"
+        coordinate={mapRegion}
+        />
+      </MapView>
     </View>
   );
 }
