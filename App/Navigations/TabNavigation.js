6d8354e0fe65331bbd6f1cd762cc3dbb2961bf9b
Desire <desireirankundawilliams@gmail.com>
2023-09-26 12:08:36 +0200

    added home screen icon

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -2,13 +2,27 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../Screens/Home";
 import Fav from "../Screens/Fav";
+import Profile from "../Screens/Profile";
+import Search from "../Screens/Search";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function TabNavigation() {
   const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={24} color="black" />
+          ),
+        }}
+      />
       <Tab.Screen name="Fav" component={Fav} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
   );
 }
