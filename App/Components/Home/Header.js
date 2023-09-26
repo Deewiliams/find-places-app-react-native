40700986a1253f704ee8cee55017439c29a6c706
Desire <desireirankundawilliams@gmail.com>
2023-09-26 12:41:37 +0200

    added assets and fonts

--- App/Components/Home/Header.js
+++ App/Components/Home/Header.js
@@ -1,10 +1,16 @@
- import { View, Text } from 'react-native'
- import React from 'react'
- 
- export default function Header() {
-   return (
-     <View>
-       <Text>Header</Text>
-     </View>
-   )
- }
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Header() {
+  return (
+    <View>
+      <Image style={styles.logo} source={require("../../../assets/logo.png")} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  logo: {
+    width: 50,
+    height: 50,
+  },
+});
