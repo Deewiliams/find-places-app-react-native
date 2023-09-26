6b019ff71a62a1465c57bbb04cd7cbef6d30a921
Desire <desireirankundawilliams@gmail.com>
2023-09-26 12:21:24 +0200

    created a navaigation bar page

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -5,24 +5,53 @@ import Fav from "../Screens/Fav";
 import Profile from "../Screens/Profile";
 import Search from "../Screens/Search";
 import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabNavigation() {
   const Tab = createBottomTabNavigator();
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
+      {/* created a navigation icon buttons */}
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
           tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="home" size={24} color="black" />
+            <FontAwesome name="home" size={size} color={color} />
           ),
         }}
       />
-      <Tab.Screen name="Fav" component={Fav} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="Fav"
+        component={Fav}
+        options={{
+          tabBarLabel: "Fav",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-sharp" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user-circle-o" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="search" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
