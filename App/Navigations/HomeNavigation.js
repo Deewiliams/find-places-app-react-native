be027b8cb869cc10b54e40225e576ac8d4c80018
Desire <desireirankundawilliams@gmail.com>
2023-09-28 10:28:36 +0200

    added detail page navigation screen

--- App/Navigations/HomeNavigation.js
+++ App/Navigations/HomeNavigation.js
@@ -1,19 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  TransitionPresets,
+  createStackNavigator,
+} from "@react-navigation/stack";
 import Home from "../Screens/Home";
-import PlaceDetails from '../Components/Home/placeDetails/PlaceDetails';
+import PlaceDetails from "../Components/Home/placeDetails/PlaceDetails";
 
 const HomeNavigation = () => {
-    const Stack = createStackNavigator()
+  const isAndroid = true;
+  const Stack = createStackNavigator();
   return (
-    <Stack.Navigator>
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="place-detail" component={PlaceDetails} />
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        headerShown: false,
+        ...(isAndroid && TransitionPresets.ModalSlideFromBottomIOS),
+      }}
+    >
+      <Stack.Screen name="home" component={Home} />
+      <Stack.Screen
+        name="place-detail"
+        component={PlaceDetails}
+        screenOptions={{
+          presentation: "model",
+        }}
+      />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default HomeNavigation
+export default HomeNavigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
